e718309b61b376298b533f3bab8889001f6d680e
Maduki-tech <d.schlueter1011@gmail.com>
2024-03-06 19:25:26 +0100

    generate basic windwo

--- lua/java_generator/init.lua
+++ lua/java_generator/init.lua
@@ -1,4 +1,8 @@
-
 local M = {}
+local ui = require("java_generator.ui")
 
+function M.setup()
+	vim.keymap.set("n", "<leader>ta", ui.toggle_quick_menu, {silent = true})
+end
 
+return M;
---
+++ lua/java_generator/ui.lua
@@ -0,0 +1,61 @@
+local GeneratorUi = {}
+
+Generator_win_id = nil
+Generator_bufh = nil
+
+local function close_menu(force_save)
+    force_save = force_save or false
+
+    vim.api.nvim_win_close(Generator_win_id, true)
+
+    Generator_win_id = nil
+    Harpoon_bufh = nil
+end
+
+local function create_window()
+    local width = 60
+    local height = 10
+    local bufnr = vim.api.nvim_create_buf(false, false)
+    local win_id =
+        vim.api.nvim_open_win(
+        bufnr,
+        false,
+        {
+            relative = "editor",
+            title = "Generator",
+            row = math.floor(((vim.o.lines - height) / 2) - 1),
+            col = math.floor((vim.o.columns - width) / 2),
+            height = height,
+            width = width,
+            style = "minimal",
+            border = "single"
+        }
+    )
+
+    if win_id == 0 then
+        vim.api.nvim_buf_delete(bufnr, {force = true})
+    end
+
+    Generator_win_id = win_id
+
+    vim.api.nvim_set_option_value("number", true, {win = win_id})
+
+    return {
+        bufnr = bufnr,
+        win_id = Generator_win_id
+    }
+end
+
+function GeneratorUi:toggle_quick_menu()
+    if Generator_win_id ~= nil and vim.api.nvim_win_is_valid(Generator_win_id) then
+        close_menu()
+        return
+    end
+
+    local win_info = create_window()
+
+    Generator_win_id = win_info.win_id
+    Generator_bufh = win_info.bufnr
+end
+
+return GeneratorUi
